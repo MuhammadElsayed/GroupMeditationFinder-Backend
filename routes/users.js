49485da66038a2d16a52ec868a95faadf59c5cdf
muhammadelsayed <msayed@dualparadox.com>
2018-02-05 10:55:22 -0600

    update error response.

--- routes/users.js
+++ routes/users.js
@@ -45,7 +45,7 @@ function authenticate(req, res) {
             if (user) { //authenticated
                 res.send(user);
             } else {
-                res.status(400).send('Email or password is incorrect');
+                res.status(400).send({error_code:1, msg:'Email or password is incorrect'});
             }
         })
         .catch(function (err) {
